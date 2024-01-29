dc730fcd5d4e1a73ecc4839ed472eaefb79b9187
jonathanlopez <toribioj818@gmail.com>
2024-01-28 22:43:08 -0400

    Added Spring Security config

---
+++ src/main/java/com/budgetproject/cloudgateway/security/OktaOAuth2WebSecurity.java
@@ -0,0 +1,36 @@
+package com.budgetproject.cloudgateway.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebFluxSecurity
+public class OktaOAuth2WebSecurity {
+
+    @Bean
+    public SecurityWebFilterChain securityFilterChain(ServerHttpSecurity http) {
+//        http
+//                .authorizeExchange(exchange -> exchange
+//                                .anyExchange().authenticated()
+//                )
+//                .oauth2Login(withDefaults())
+//                .oauth2ResourceServer(oauth2ResourceServer -> oauth2ResourceServer
+//                                .jwt(jwt -> {})
+//                );
+
+        http
+                .authorizeExchange(exchange -> exchange
+                        .anyExchange().authenticated()
+                )
+                .oauth2Login(withDefaults())
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
+
+        return http.build();
+    }
+}
